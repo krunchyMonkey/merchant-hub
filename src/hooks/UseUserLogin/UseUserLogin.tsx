e7bbf7431eb18eb940aae9d5e92bbb2594855cbe
Kristoffer LaRue <kristoffer.larue@vivint.com>
2024-02-03 01:34:41 -0700

    connected to docker service

--- src/hooks/UseUserLogin/UseUserLogin.tsx
+++ src/hooks/UseUserLogin/UseUserLogin.tsx
@@ -1,24 +1,36 @@
 import { updateLoginInfo } from 'src/store/reducers/UserInfoReducer';
 import { useUserInfoContext } from 'src/store/slices/UserInfoSlice';
 import { useCallback } from 'react';
+import axios from 'axios';
 
 export type userLoginResult = {
-    dispatchUserLogin: (newUsernameValue: string, newPassword: string) => void;
+  dispatchUserLogin: (newUsernameValue: string, newPassword: string) => void;
 };
 
 function useUserLogin(): userLoginResult {
-  const {
-    dispatch: userLoginDispatch,
-  } = useUserInfoContext();
+  const { dispatch: userLoginDispatch } = useUserInfoContext();
 
-  const dispatchUserLogin = useCallback((newUsername: string, newPassword: string) => {
-    userLoginDispatch(updateLoginInfo('username', newUsername));
-    userLoginDispatch(updateLoginInfo('password', newPassword));
-    // eslint-disable-next-line no-console
-    console.log(newUsername);
-    // eslint-disable-next-line no-console
-    console.log(newPassword);
-  }, [userLoginDispatch]);
+  const dispatchUserLogin = useCallback(
+    (newUsername: string, newPassword: string) => {
+      userLoginDispatch(updateLoginInfo('username', newUsername));
+      userLoginDispatch(updateLoginInfo('password', newPassword));
+      // eslint-disable-next-line no-console
+      axios
+        .post('https://localhost:32770/login', {
+          username: newUsername,
+          password: newPassword,
+        })
+        .then((response) => {
+          // eslint-disable-next-line no-console
+          console.log(response.data);
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log(error);
+        });
+    },
+    [userLoginDispatch],
+  );
 
   return {
     dispatchUserLogin,
